317c3d96813cc7165bf87acc831a0bb2b2fcb6eb
Rachana Blake <rachanasor222@gmail.com>
2022-10-01 08:26:43 +0700

    Change navigaion, working on new page

--- style/home.js
+++ style/home.js
@@ -5,4 +5,17 @@ window.onscroll = function (e) {
     else {
         document.querySelector('#top').classList.remove('gotop');
     }
-}
+  }
+  
+  // Scroll Hide Navigation-----------------------------------------------------------------------
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("scrolled").style.top = "0";
+    } else {
+      document.getElementById("scrolled").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+  
